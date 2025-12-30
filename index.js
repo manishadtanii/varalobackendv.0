f62ff0ddabcff8579b38d399533bb0509ecef824
manishadtanii <Manish@digicots.com>
2025-12-30 07:35:47 -0500

    redeploy

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const port = process.env.PORT || 3000;
 
 // CORS Configuration
 const corsOptions = {
-  origin: ['https://552a721652e2.ngrok-free.app','http://localhost:5173', 'http://localhost:3000', 'http://127.0.0.1:5173'],
+  origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://varallocms.vercel.app'],
   credentials: true,
   optionsSuccessStatus: 200,
 };
