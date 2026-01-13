0d5e9653182929b3bbc423b735a591d785fed0c6
manishadtanii <Manish@digicots.com>
2026-01-13 05:29:50 -0500

    changes on urllll

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost:5174',
-  'https://varallofrontendv-0.vercel.app/',
+  'https://varallofrontendv-0.vercel.app',
 ];
 
 app.use(
