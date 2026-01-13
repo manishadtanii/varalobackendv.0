0c1d596dcab3b07536254f6c5e55c3979673dadf
manishadtanii <Manish@digicots.com>
2026-01-13 05:22:59 -0500

    changes done on index

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost:5174',
-  'https://varallocms.vercel.app',
-  'https://frontendcmsv1.vercel.app',
+  'https://varallofrontendv-0.vercel.app/',
 ];
 
 app.use(
